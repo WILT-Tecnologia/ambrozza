762f8d2e7ede2237c8130fb27e159def32e8c477
TelmoPersch <119141423+telmopersch123@users.noreply.github.com>
2026-08-13 22:32:35 -0400

    feat: adiciona componentes de mais vendidos e categorias com layout responsivo

--- frontend/src/app/features/home/presentation/pages/home-page/home-page.component.ts
+++ frontend/src/app/features/home/presentation/pages/home-page/home-page.component.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CategoriasHomeComponent } from '../../components/categorias/categoriasHome.component';
 import { DestaquesComponent } from '../../components/destaques/destaques.component';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { InfoCardsComponent } from '../../components/InfoCards/infocards.component';
+import { MaisVendidosComponent } from '../../components/mais-vendidos/mais-vendidos.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, HeroComponent, InfoCardsComponent, DestaquesComponent],
+  imports: [
+    CommonModule,
+    HeroComponent,
+    InfoCardsComponent,
+    DestaquesComponent,
+    MaisVendidosComponent,
+    CategoriasHomeComponent,
+  ],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {}
